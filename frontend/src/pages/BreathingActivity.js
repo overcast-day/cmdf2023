b6f98051fdd4f05a30154dd8d3234fb137d2e2a2
diana liang <aforestsmoon@gmail.com>
2023-03-12 07:59:43 -0700

    scroll animation for breathing activity

--- frontend/src/pages/BreathingActivity.js
+++ frontend/src/pages/BreathingActivity.js
@@ -3,30 +3,49 @@ import React from 'react'
 import Timer from '../components/Timer.js';
 import { Link, useHistory } from "react-router-dom";
 import { useRef, useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
 
 export default function Breathe() {
+    const handleClickScroll = () => {
+        const element = document.getElementById('bottom-section');
+        if (element) {
+          element.scrollIntoView({behavior: 'smooth' });
+        }
+    };
     const handleClick = (e) => {
-       window.location("/dashboard");
-       e.preventDefault();
+        window.close();
     };
 
     return (
-        <div className="Breathing">
-        <div>
-            <h1>Breathe</h1>
-            <Timer 
-                h = {0}
+        <div className="Breathe">
+        <div id = "top-section">
+            <div class="whole-page">
+            <h2>It's time for a break! Let's do a </h2>
+            <h1>Breathing Exercise </h1>
+            <h3>5 minutes</h3>
+            <p>Take some time to take deep breaths</p>
+            <button 
+                className="scroll-button"
+                class="confirm-button" 
+                onClick={handleClickScroll}>
+                    Start
+            </button></div>
+        </div>
+        <div id = "bottom-section">
+            <div class="whole-page">
+            <h2> 1,2,3...in...1,2,3....out. Take this time to be in the moment</h2>
+            <Timer className="small-timer"
+                h = {1}
                 m = {0}
                 s = {3}
                 link = "/dashboard"
             />
+            <h2>remaining </h2>
             <button 
                 class="confirm-button" 
                 onClick={handleClick}>
-                    Back to dashboard
+                Return to dashboard
             </button>
-        </div>
+        </div></div>
         </div>
     );
 }
