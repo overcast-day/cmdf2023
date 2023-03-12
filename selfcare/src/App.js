720db707a5d608ef0a03000e055162a584c0a804
diana liang <aforestsmoon@gmail.com>
2023-03-11 16:53:56 -0800

    create pages with router

--- selfcare/src/App.js
+++ selfcare/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import {Components} from './components'
+
+import StartPage from './pages/StartPage'
+import Dashboard from './pages/Dashboard'
+import SchedulePage from './pages/SchedulePage'
+import BreathingActivityPage from './pages/BreathingActivity'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/components" element={<Components/>}/>
+      <Route path="/start" element={<StartPage/>}/>
+      <Route path="/schedule" element={<SchedulePage/>}/>
+      <Route path="/breathing_activity" element={<BreathingActivity/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
   );
 }
 
