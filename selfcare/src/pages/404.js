f363fe85a81c7798cd7380e6d447ecfdfffc02a9
jaclyncanlas <canlasjaclyn@gmail.com>
2023-03-11 22:47:34 -0800

    formatted error page

--- selfcare/src/pages/404.js
+++ selfcare/src/pages/404.js
@@ -1,15 +1,20 @@
 import '../App.css';
 import React from 'react'
 import ErrorLogo from "../images/sad_dog.png";
+import Button from "react-bootstrap/Button"
+
 
 export default function Error() {
     return (
         <div className="ErrorPage">
         <div>
             <p>Oh no! The page you're looking for doesn't exist. </p>
-        <img src={ErrorLogo} alt = "sad_dog_pouting" />
+            <p>404 Not Found</p>
+            <img src={ErrorLogo} alt = "sad_dog_pouting" />
+            <a href ="/">
+                <Button>Back to Home Page</Button>
+            </a>
         </div>
         </div>
     );
 }
-
