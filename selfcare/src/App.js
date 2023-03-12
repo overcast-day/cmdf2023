5e54b0fe333ef42035a77470b919a1964a842b61
diana liang <aforestsmoon@gmail.com>
2023-03-11 18:13:36 -0800

    fixed a few variable errors

--- selfcare/src/App.js
+++ selfcare/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 import StartPage from './pages/StartPage'
 import Dashboard from './pages/Dashboard'
@@ -12,12 +12,11 @@ import PageNotFound from './pages/404'
 function App() {
   return (
     <Routes>
-      <Route path="/components" element={<Components/>}/>
       <Route path="/start" element={<StartPage/>}/>
       <Route path="/schedule" element={<SchedulePage/>}/>
       <Route path="/breathing_activity" element={<BreathingActivity/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/404" element={<PageNotFound/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
     </Routes>
   );
 }
