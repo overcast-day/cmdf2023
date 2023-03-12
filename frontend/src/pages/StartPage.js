2aa1d0de7df931d55fcda74966ca7afc0ec8b169
diana liang <aforestsmoon@gmail.com>
2023-03-12 11:17:41 -0700

    scroll ui for start page

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -2,19 +2,64 @@ import '../App.css';
 import React from 'react'
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
 
 export default function StartPage() {
+    const handleClickScroll = () => {
+        const element = document.getElementById('bottom-section');
+        if (element) {
+          element.scrollIntoView({behavior: 'smooth' });
+        }
+    };
+
+    const handleClick = (e) => {
+        window.location.href="/schedule";
+        e.preventDefault();
+    };
+
     return (
         <div className="Start">
-        <div>
+        <div id="top-section">
+            <h3>Helping you prioritize self-care throughout your day!</h3>
+            <button 
+                className="scroll-button"
+                class="confirm-button" 
+                onClick={handleClickScroll}>
+                    Let's get stated
+            </button>
+        </div>
+        <div id="bottom-section">
             <h1>Let's start your work day!</h1>
             <h4>Select the activities you want to include:</h4>
-            <Button href="/breathing_activity">Breathing Activity</Button>
+            <Form.Group>
+                <Form.Check type={"checkbox"}>
+                <Form.Check.Input
+                    type={"checkbox"}
+                    defaultChecked={true}
+                />
+                <Form.Check.Label>Breathing Exercise</Form.Check.Label>
+                <Form.Check.Input
+                    type={"checkbox"}
+                    defaultChecked={true}
+                />
+                <Form.Check.Label>Water Break</Form.Check.Label>
+                <Form.Check.Input
+                    type={"checkbox"}
+                    defaultChecked={true}
+                />
+                <Form.Check.Label>Surprise Me!</Form.Check.Label>
+            </Form.Check>
+            </Form.Group>
             <br />
             <h4>How many hours are you working for?</h4>
             <h4>How often do you want a break?</h4>
-            <Button href="/schedule">schedule</Button>
+            <button 
+                class="confirm-button" 
+                onClick={handleClick}>
+                Next
+            </button>
             <br />
+            <p></p>
             <Link to="/404">404</Link>
         </div>
         </div>
